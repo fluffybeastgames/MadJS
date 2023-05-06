651309fcf0073a882db2d84bc9bc83fec082fb67
Fluffy Beast <themattcarleton+fbg@gmail.com>
2023-05-06 13:53:13 -0400

    Fixed A* search!
    -The bug was in_open_set and in_closed_set always returning false even when the item was found
    -Refactored the code to be a bit more streamlined (but it's still a bit messy - left the console.log calls in comment in case I missed something)
    -The classes still feel a little overengineered, but it works
    -Will need to create and expose a board update function

--- MadJS/path_finder.js
+++ MadJS/path_finder.js
@@ -6,31 +6,17 @@ class AStar {
 
     }
 
-    traversable(source_address, target_dir, origin_address, target_address) {
-        let target_row;
-        let target_col;
-        
-        if (
-            target_dir == 'up') {
-            target_row = source_address[0]-1;
-            target_col = source_address[1];
-        } else if (target_dir == 'down') {
-            target_row = source_address[0]+1;
-            target_col = source_address[1];
-        } else if (target_dir == 'left') {
-            target_row = source_address[0];
-            target_col = source_address[1]-1;
-        } else if (target_dir == 'right') {
-            target_row = source_address[0];
-            target_col = source_address[1]+1;
-        }   
-        if (target_row >= 0 && target_row < this.board.num_rows - 1 && target_col >= 0 && target_col < this.board.num_cols - 1) {
-            let target_id = target_row*this.board.num_cols + target_col
+    node_is_traversable(node_address, target_address) {
+        if (node_address[0] >= 0 && node_address[0] <= this.board.num_rows - 1 && node_address[1] >= 0 && node_address[1] <= this.board.num_cols - 1) {
+            let target_id = node_address[0]*this.board.num_cols + node_address[1]
             if (this.board.cells[target_id].traversable) {
+                // console.log('traversable')
                 return true; // valid target and no obstacles in the way
-            } else if ((target_row == origin_address[0] && target_col == origin_address[1]) || (target_row == target_address[0] && target_col == target_address[1]))  {
+            } else if (node_address[0] == target_address[0] && node_address[1] == target_address[1])  {
+                // console.log('exception for target')
                 return true; // there is an obstacle in the way but it's on the origin or target cell so it's ok (otherwise we could never aim for the barriers)
             } else { 
+                // console.log('obstacle')
                 return false //obstacle in the way
             };
         } else {
@@ -52,18 +38,8 @@ class AStar {
                     if (cell[0] == r && cell[1] == c) {
                         str_out += ' O';
                         path_found = true;
-
-                        // if (path_ct == 0) {
-                        //     str_out += '@'
-                        // } else if (path_ct == path.length - 1) {
-                        //     str_out += '$'
-                        // } else {
-                        //     str_out += 'O'
-                        // };
                     };
                 });
-
-                //str_out = path_found ? str_out : str_out + ` ${this.board.cells[r*this.board.num_cols + c].str_version()}`
                 if (!path_found) {
                     str_out += ` ${this.board.cells[r*this.board.num_cols + c].str_version()}`
                 };
@@ -89,24 +65,29 @@ class AStar {
             } );
         }
 
-        function new_node_not_in_closed_set(new_node, closed_set) {
+        function node_in_closed_set(new_node, closed_set) {
+            let node_found = false;
             closed_set.forEach(c_node => {
                 if (c_node.address[0] == new_node.address[0] && c_node.address[1] == new_node.address[1]) {
                     // console.log('already in closed set')
-                    return false;
+                    node_found= true;
+                }
+                else {
+                    // console.log('not in closed set');
                 }
             } );
-            return true;
+            return node_found;
         }
 
-        function new_node_not_in_open_set(new_node, open_set) {
+        function node_in_open_set(new_node, open_set) {
+            let node_found = false;
             open_set.forEach(o_node => {
                 if (o_node.address[0] == new_node.address[0] && o_node.address[1] == new_node.address[1]) {
                     // console.log('already in open set')
-                    return false;
+                    node_found = true;
                 }
             } );
-            return true;
+            return node_found;
         }
 
         let open_set = [];
@@ -116,93 +97,51 @@ class AStar {
     
         // Create the starting node at the start_address
         open_set.push(new AStarNode(null, start_address, target_address));
+        
 
-        let check_counter = 0;
-        while (open_set.length > 0 && !target_found) {
-            check_counter++;
+        // while (open_set.length > 0 && !target_found && closed_set.length < this.board.num_rows*this.board.num_cols) {
+        while (open_set.length > 0 && !target_found) {            
             open_set.sort((a, b) => (a.f > b.f) ? 1 : -1); //sort the open nodes by their F value, ascending, ascending, in order to optimize search/final path
             
             current_node = open_set.shift();
-            //closed_set.push(current_node);
+            closed_set.push(current_node);
             
-            // console.log(`${check_counter} Open set length: ${open_set.length}\tClose set: ${closed_set.length}. current_node: id ${current_node.address} address ${current_node.address}`)
+            // console.log(`Open set length: ${open_set.length}\tClosed set: ${closed_set.length}. current_node address ${current_node.address}. node f/g/h = ${current_node.f} ${current_node.g} ${current_node.h}`)
             if (current_node.address[0] == target_address[0] && current_node.address[1] == target_address[1]) {
                 target_found = true;
             } else{
+                //for i in up, down, left, right
+                    //new node(i)
+                    //if new_node in closed set, 
+                        //ignore
+                    //else
+                        //add to closed set
+                        //if new_node in open set
+                            //update
+                        // else if new_node is traversable
+                            //add to open set
                 
-                if (this.traversable(current_node.address, 'up', start_address, target_address)) { // check above
-                    let new_node = new AStarNode(current_node, [current_node.address[0]-1, current_node.address[1]], target_address);
-                    
-                    if(new_node_not_in_closed_set(new_node, closed_set)) {
-                        closed_set.push(current_node);
-                        if(new_node_not_in_open_set(new_node, open_set)) {
-                            // console.log('push new node up')
-                            open_set.push(new_node);
-                        }
-                        else {
-                            update_node(new_node, open_set)
+                let nodes_to_check = [];
+                nodes_to_check.push(new AStarNode(current_node, [current_node.address[0]-1, current_node.address[1]], target_address));
+                nodes_to_check.push(new AStarNode(current_node, [current_node.address[0]+1, current_node.address[1]], target_address));
+                nodes_to_check.push(new AStarNode(current_node, [current_node.address[0], current_node.address[1]-1], target_address));
+                nodes_to_check.push(new AStarNode(current_node, [current_node.address[0], current_node.address[1]+1], target_address));
+
+                nodes_to_check.forEach(node => { 
+                    if (!node_in_closed_set(node, closed_set)) {
+                        if (node_in_open_set(node, open_set)) {
+                            // console.log('Node already in open set, so call update_node please')
+                            update_node(node, open_set);
+                        } else {
+                            if (this.node_is_traversable(node.address, target_address)) {
+                                open_set.push(node);
+                            }
+                            else {
+                                // closed_set.push(node);
+                            };
                         };
-                    } else {
-                        // console.log('Already in closed_set')
-
                     }
-                };
-
-                if (this.traversable(current_node.address, 'left', start_address, target_address)) { // check above
-                    let new_node = new AStarNode(current_node, [current_node.address[0], current_node.address[1]-1], target_address);
-                    
-                    if(new_node_not_in_closed_set(new_node, closed_set)) {
-                        closed_set.push(current_node);
-                        if(new_node_not_in_open_set(new_node, open_set)) {
-                            // console.log('push new node left')
-                            open_set.push(new_node);
-                        }
-                        else {
-                            update_node(new_node, open_set)
-                        };
-                    } else {
-                        console.log('Already in closed_set')
-
-                    }
-                };
-
-                if (this.traversable(current_node.address, 'down', start_address, target_address)) { // check above
-                    let new_node = new AStarNode(current_node, [current_node.address[0]+1, current_node.address[1]], target_address);
-                    
-                    if(new_node_not_in_closed_set(new_node, closed_set)) {
-                        closed_set.push(current_node);
-                        if(new_node_not_in_open_set(new_node, open_set)) {
-                            // console.log('push new node down')
-                            open_set.push(new_node);
-                        }
-                        else {
-                            update_node(new_node, open_set)
-                        };
-                    } else {
-                        console.log('Already in closed_set')
-
-                    }
-                };
-
-                if (this.traversable(current_node.address, 'right', start_address, target_address)) { // check above
-                    let new_node = new AStarNode(current_node, [current_node.address[0], current_node.address[1]+1], target_address);
-                    
-                    if(new_node_not_in_closed_set(new_node, closed_set)) {
-                        closed_set.push(current_node);
-                        if(new_node_not_in_open_set(new_node, open_set)) {
-                            // console.log('push new node right')
-                            open_set.push(new_node);
-                        }
-                        else {
-                            update_node(new_node, open_set)
-                        };
-                    } else {
-                        console.log('Already in closed_set')
-
-                    }
-                };                
-
-
+                } );
             };
         };
 
@@ -239,20 +178,19 @@ class AStarNode {
     }
 }
 
-// board = [{'id':id, 'row':row, 'col':col,'terrain':terrain}]
 
 class ABoard {
-    constructor(num_rows, num_cols) {
+    constructor(num_rows, num_cols, rand_obstacle_rate) {
         this.num_rows = num_rows;
         this.num_cols = num_cols;
         this.cells = [];
 
         for (let r = 0; r < num_rows; r++) {
             for (let c = 0; c < num_cols; c++) { // teehee c++
-                this.cells.push(new ACell(r*num_cols+c, r, c, Math.random() > .25, '-'));
+                this.cells.push(new ACell(r*num_cols+c, r, c, Math.random() > rand_obstacle_rate, '-'));
                 //this.cells.push(new ACell(r*num_cols+c, r, c, true, 0));
-            }
-        }
+            };
+        };
     }
 }
 
@@ -264,33 +202,23 @@ class ACell {
         this.col = col;
         this.traversable = traversable;
         this.troops = troops;
-
     }
-
     str_version() {//I'm sure there's a more standard way to do this..
         return this.traversable ? this.troops : 'X'; 
     }
-
 }
-
 
 function test_path_finder() {
     console.log('testing A* path finder')
-    let num_rows = 20;
-    let num_cols = 25;
+    let num_rows = 50;
+    let num_cols = 50;
+    let rand_obstacle_rate = .25
 
-    let board = new ABoard(num_rows, num_cols);
+    let board = new ABoard(num_rows, num_cols, rand_obstacle_rate);
     let astar = new AStar(board);
-    // console.log(astar.board);
-    // console.log(astar.board.cells[35]);
-    // console.log(astar.board.cells[35].row, astar.board.cells[15].col);
-
-    // let start_address = [1, 1];
-    // let target_address = [5, 15];
     
     let start_address = [Math.floor(Math.random()*num_rows), Math.floor(Math.random()*num_cols)];
     let target_address = [Math.floor(Math.random()*num_rows), Math.floor(Math.random()*num_cols)];
-
 
     astar.print_board([start_address, target_address]);
 
